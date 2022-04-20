226903e5c2af6b6dfa3bf4de283727617de8a6f0
Osmar Neto <osmar.neto23@gmail.com>
2022-04-20 02:39:53 -0300

    Refactory

--- JogoDaVelha/script.js
+++ JogoDaVelha/script.js
@@ -1,7 +1,7 @@
 /* Player 1 Mark */
-const markPlayer1 = 'X';
+const player1Marker = 'X';
 /* Player 2 Mark */
-const markPlayer2 = 'O';
+const player2Marker = 'O';
 /* Default mark */
 const defaultMark = '';
 /* Player counter*/
@@ -14,106 +14,135 @@ ResetSquares();
 function AnnounceWinner(){
     if(currentPlayer === 0){
         winnerPlayer = 0;
-        document.getElementById('winnerMark').innerText = markPlayer1;
+        document.getElementById('winnerMarker').innerText = player1Marker;
     }
     else{
         winnerPlayer = 1;
-        document.getElementById('winnerMark').innerText = markPlayer2;
+        document.getElementById('winnerMarker').innerText = player2Marker;
     }    
 }
-function CheckRowForWinner(squareList,  squareIndex, squareText) {
-    let initialIndex = 0;
-    let finalIndex = 0;
-    let sequelCounter = 0;
-
-    if(squareIndex <= 2){
-        initialIndex = 0;
-        finalIndex = 3;
-    }
-    else if(squareIndex >=3 && squareIndex <=5) {
-        initialIndex = 3;
-        finalIndex = 6;
-    }
-    else {
-        initialIndex = 6;
-        finalIndex = 9;
-    }
-
-    for (let i = initialIndex; i < finalIndex; i++) {
-        let neighborSquareText =  document.getElementById(squareList[i].id).innerText;
-        if(neighborSquareText === squareText){
-            sequelCounter++;
-        }
-    }
-
-    if(sequelCounter === 3){
-        for (let i = initialIndex; i < finalIndex; i++) {
-            document.getElementById(squareList[i].id).style.backgroundColor = "green";
-        }
-        return true;
-    }
-    else{
-        return false;
+function PaintSquares(squaresList,indexes){
+    /*Paint Square*/
+    for (let i = 0; i < indexes.length; i++)
+    {
+        document.getElementById(squaresList[indexes[i]].id).style.backgroundColor = "green";
     }
 }
-function CheckColForWinner(squareList,  squareIndex, squareText){
-    let initialIndex = 0;
-    let finalIndex = 0;
-    let sequelCounter = 0;
-
-
-    if(squareIndex === 0 || squareIndex === 3 || squareIndex === 6){
-        initialIndex = 0;
-        finalIndex = 6;
+function GetSquaresTexts(squaresList){
+    let squaresTextList = [];
+    for (let index = 0; index < squaresList.length; index++){
+        let itemText = squaresList[index].innerHTML;
+        squaresTextList.push(itemText);
     }
-    else if(squareIndex === 1 || squareIndex === 4 || squareIndex === 7)
-    {
-        initialIndex = 1;
-        finalIndex = 7;
+    return squaresTextList;
+}
+function GetSquareNeighbors(squareList, squareIndex, lineStyle){
+    if(squareIndex < 0 || squareIndex > squareList.length - 1 || typeof squareIndex === undefined || typeof squareList === undefined || typeof lineStyle === undefined){
+        return;
     }
-    else
-    {
-        initialIndex = 2;
-        finalIndex = 8;
+    let neighborsIndexes = [];
+
+    if(lineStyle === 'Row'){
+        /* Row */
+        if(squareIndex <= 2){
+            /* First Row*/
+            neighborsIndexes.push(0,1,2);
+        }else if(squareIndex > 2 && squareIndex <= 5){
+            /* Second Row*/
+            neighborsIndexes.push(3,4,5);
+        }else{
+            /* Third Row*/
+            neighborsIndexes.push(6,7,8);
+        }
+    }else if(lineStyle === 'Column'){
+        /* Column */
+        if(squareIndex === 0 || squareIndex === 3 || squareIndex === 6 ){
+            /* First Column */
+            neighborsIndexes.push(0,3,6);
+        }else if(squareIndex === 1 || squareIndex === 4 || squareIndex === 7){
+            /* Second Column*/
+            neighborsIndexes.push(1,4,7);
+        }else{
+            /* Third Column*/
+            neighborsIndexes.push(2,5,8);
+        }
+    }else if(lineStyle === 'Diagonal'){
+        if(squareIndex === 0 || squareIndex === 8 ){
+            /* \ */
+            neighborsIndexes.push(0,4,8);
+        }else if(squareIndex === 2 || squareIndex === 6){
+            /* / */
+            neighborsIndexes.push(2,4,6);
+        }else if (squareIndex === 4){
+            /* X */
+            neighborsIndexes.push(0,2,4,6,8);
+        }else{
+            /* No diagonal Neighbors */
+            return;
+        }
+    }else {
+        /* No a valid input */
+        return;
     }
 
-    for (let i = initialIndex; i <= finalIndex; i+=3)
-    {
-        let neighborSquareText =  document.getElementById(squareList[i].id).innerText;
-        if(neighborSquareText === squareText){
-            sequelCounter++;
+    /* Return Neighbors*/
+    console.log(neighborsIndexes);
+    return new Set(neighborsIndexes);
+}
+function CheckSquareStreak(squaresList, squareIndex, lineStyle){
+    let squaresTexts = [...GetSquaresTexts(squaresList)];
+    let neighborsSquares = [...GetSquareNeighbors(squaresList, squareIndex, lineStyle)];
+    if(neighborsSquares.length === 3){
+        if(
+            squaresTexts[neighborsSquares[0]] === squaresTexts[neighborsSquares[1]] &&
+            squaresTexts[neighborsSquares[0]] === squaresTexts[neighborsSquares[2]] &&
+            squaresTexts[neighborsSquares[1]] === squaresTexts[neighborsSquares[2]]
+        ){
+            PaintSquares(squaresList, neighborsSquares);
+            AnnounceWinner();
         }
     }
+    else{
+       if (lineStyle === 'Diagonal' && squareIndex === 4){
+           let diagIndexes = [];
+           if(
+               squaresTexts[neighborsSquares[0]] === squaresTexts[neighborsSquares[2]] &&
+               squaresTexts[neighborsSquares[0]] === squaresTexts[neighborsSquares[4]] &&
+               squaresTexts[neighborsSquares[2]] === squaresTexts[neighborsSquares[4]]
+           ){
+               diagIndexes.push(0,2,4);
+           }
 
-    if(sequelCounter === 3){
-        for (let i = initialIndex; i <= finalIndex; i+=3) {
-            document.getElementById(squareList[i].id).style.backgroundColor = "green";
-        }
-        return true;
-    }else{
-        return false;
+           if(
+               squaresTexts[neighborsSquares[1]] === squaresTexts[neighborsSquares[2]] &&
+               squaresTexts[neighborsSquares[1]] === squaresTexts[neighborsSquares[3]] &&
+               squaresTexts[neighborsSquares[2]] === squaresTexts[neighborsSquares[3]]
+           ){
+               diagIndexes.push(1,2,3);
+           }
+
+           if(diagIndexes.length > 0){
+               PaintSquares(squaresList, diagIndexes);
+               AnnounceWinner();
+           }
+       }
     }
 
 }
 function CheckForWinner(square){
     let squaresList = document.getElementsByClassName('square');
     let squareIndex = -1;
-    for (let count = 0; count < squaresList.length; count++){
-        if(squaresList[count] === square){
-            squareIndex = count;
-            break;
+    for (let i = 0; i < squaresList.length; i++){
+        if(squaresList[i] === square){
+            squareIndex = i;
         }
     }
-    let squareText = document.getElementById(square.id).innerText;
-    /* Check Row */
-    if(CheckRowForWinner(squaresList, squareIndex, squareText)){
-        AnnounceWinner(currentPlayer);
-    }
-    else
-    {
-        /* Check Column */
-        if(CheckColForWinner(squaresList, squareIndex, squareText)){
-            AnnounceWinner(currentPlayer);
+
+    if(squareIndex >= 0 && squareIndex < squaresList.length){
+        CheckSquareStreak(squaresList, squareIndex, 'Row');
+        CheckSquareStreak(squaresList, squareIndex, 'Column');
+        if (squareIndex !== 1 && squareIndex !== 3  && squareIndex !== 5  && squareIndex !== 7){
+            CheckSquareStreak(squaresList, squareIndex, 'Diagonal');
         }
     }
 }
@@ -125,36 +154,36 @@ function PickSquare(square) {
         /* Get square current value */
         let squareText = document.getElementById(square.id).innerText;
         /* Is Square available? */
-        if(squareText !== markPlayer1 && squareText !== markPlayer2)
+        if(squareText !== player1Marker && squareText !== player2Marker)
         {
             /* Who is the current player ? */
             if(currentPlayer === 0)
             {
                 /*If player 1 put mark 1*/
-                mark = markPlayer1;
+                mark = player1Marker;
 
             }else
             {
                 /*If player 2 put mark 2*/
-                mark = markPlayer2;
+                mark = player2Marker;
             }
             /*Insert text or image  */
-            document.getElementById(square.id).innerText = mark;
+            document.getElementById(square.id).innerHTML = mark;
             CheckForWinner(square);
             NextRound();
         }
     }
 }
 function ResetSquares(){
-    let squareList = document.getElementsByClassName('square');
-    for (let count = 0; count < squareList.length; count++){
-        document.getElementById(squareList[count].id).innerText = defaultMark;
-        document.getElementById(squareList[count].id).style.backgroundColor = "#eee";
+    let squaresList = document.getElementsByClassName('square');
+    for (let count = 0; count < squaresList.length; count++){
+        document.getElementById(squaresList[count].id).innerText = defaultMark;
+        document.getElementById(squaresList[count].id).style.backgroundColor = "#eee";
     }
     winnerPlayer = -1;
     currentPlayer = 0;
-    document.getElementById("winnerMark").innerText = defaultMark;
-    document.getElementById("playerMark").innerText = markPlayer1;
+    document.getElementById("winnerMarker").innerText = defaultMark;
+    document.getElementById("currentPlayerMarker").innerText = player1Marker;
 }
 function NextRound() {
     if(winnerPlayer === -1){
@@ -163,14 +192,14 @@ function NextRound() {
             /*Update player counter*/
             currentPlayer = 1;
             /*Update UI*/
-            document.getElementById('playerMark').innerText = markPlayer2;
+            document.getElementById('currentPlayerMarker').innerText = player2Marker;
         }
         else
         {
             /*Update player counter*/
             currentPlayer = 0;
             /*Update UI*/
-            document.getElementById('playerMark').innerText = markPlayer1;
+            document.getElementById('currentPlayerMarker').innerText = player1Marker;
         }
     }
 }
